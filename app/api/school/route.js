f35c045abac0738566a8f75a467d66057cb5aae2
Anshul <anshulraturi007@gmail.com>
2025-09-03 21:10:53 +0530

    create post api route for school data insertion

--- app/api/school/route.js
+++ app/api/school/route.js
@@ -1,1 +1,16 @@
+import prisma from "../../lib/prisma";
 
+export async function POST(req) {
+  try {
+    const body = await req.json();
+    const { name, address, city, state, contact, email_id, image } = body;
+
+    const school = await prisma.school.create({
+      data: { name, address, city, state, contact, email_id, image },
+    });
+
+    return Response.json(school, { status: 200 });
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}
